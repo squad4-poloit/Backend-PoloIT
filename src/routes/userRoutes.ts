fd54bdeb75330aad8274e257e84cfcdf5700b0f9
cris-vr <cristianvargas.dev@gmail.com>
2024-09-04 17:29:50 -0300

    feat(routes): :truck: add routes for mentorship

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import UserController from "../controllers/usersController";
+import UserController from "../controllers/userController";
 
 const router = Router();
 
 router.get("/users", UserController.getUser);
 router.get("/users/:id", UserController.getUser);
 router.post("/users", UserController.createUser);
-router.delete("/users/:id",UserController.deleteUser );
+router.delete("/users/:id", UserController.deleteUser);
 router.patch("/users/:id", UserController.updateUser);
 
 export default router;
