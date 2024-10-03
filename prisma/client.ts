bd17de1e18db8f956929ea11201b8784251c6530
cris-vr <cristianvargas.dev@gmail.com>
2024-10-03 19:00:35 -0300

    chore: jenkins test3

--- prisma/client.ts
+++ prisma/client.ts
@@ -1,8 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient({
-	datasourceUrl: process.env.DATABASE_URL,
-});
+const prisma = new PrismaClient();
 
 console.log(
 	"🚀 PrismaClient ha sido inicializado y está listo para interactuar con la base de datos.",
