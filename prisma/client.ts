91c65b14203fa50dffa824d5fbffdf684fa237f0
cris-vr <cristianvargas.dev@gmail.com>
2024-10-06 17:52:51 -0300

    test deploy 3

--- prisma/client.ts
+++ prisma/client.ts
@@ -5,5 +5,6 @@ const prisma = new PrismaClient();
 console.log(
 	"🚀 PrismaClient ha sido inicializado y está listo para interactuar con la base de datos.",
 );
+console.log("database url: ", process.env.DATABASE_URL);
 
 export default prisma;
