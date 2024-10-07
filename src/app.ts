9bacc7a315b90e06607ceb7e140f984fc8c1bcb6
cris-vr <cristianvargas.dev@gmail.com>
2024-10-07 14:48:29 -0300

    chore: :fire: remove unnecessary code

--- src/app.ts
+++ src/app.ts
@@ -2,37 +2,17 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import { router } from "src/routes";
-import type { Request, Response, NextFunction } from "express";
-import type { CustomError } from "@interfaces/error.interface";
 import { errorHandler } from "./middlewares/errorHandler";
 const PORT = process.env.PORT || 3000;
 const NODE_ENV = process.env.NODE_ENV || "development";
 
 const app = express();
-
 app.use(cors());
-
 app.use(express.json());
-
-app.use(
-	(err: CustomError, _req: Request, res: Response, next: NextFunction) => {
-		if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
-			console.error(err);
-			return res.status(400).send({ status: 400, message: err.message });
-		}
-		return next();
-	},
-);
 
 app.use(morgan("dev"));
 
 app.use("/api", router);
-
-app.get("/api/error", (_req: Request, _res: Response, next: NextFunction) => {
-	// Aquí generamos un error intencionalmente
-	const error = new Error("This is an intentional error");
-	next(error); // Pasamos el error al siguiente middleware (el manejador de errores)
-});
 
 app.get("/", (_req, res) => {
 	res.send("<h2>Api Sistema de Gestión de inscripciones PoloIT</h2>");
